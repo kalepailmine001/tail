94860ba3b49ecddb7cce2de743fbe97f3e3ba582
kalepailmine001 <itssarari+999@gmail.com>
2025-08-03 18:27:21 +0530

    Update vid.sh

--- vid.sh
+++ vid.sh
@@ -1,4 +1,4 @@
-curl 'https://www.bitcoin-earn.com/ajax.php' \
+curl -s 'https://www.bitcoin-earn.com/ajax.php' \
   -b 'PHPSESSID=0k13ed06mbunlulpqfhjnapgap' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36' \
   --data-raw 'action=claim'
